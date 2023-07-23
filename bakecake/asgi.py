fe1ca7959e5a910affcdf9f0f12c30bfeee4a997
AndreyShukatka <106096891+AndreyShukatka@users.noreply.github.com>
2023-07-23 14:45:40 +0300

    Переименовал проект

--- bakecake/asgi.py
+++ bakecake/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for BakeCake project.
+ASGI config for bakecake project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BakeCake.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bakecake.settings')
 
 application = get_asgi_application()
--- bakecake/urls.py
+++ bakecake/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for BakeCake project.
+URL configuration for bakecake project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
